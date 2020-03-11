084a7f03285f513f209fa3c82718427edd51dabf
danielvfo <danielvfo@gmail.com>
2020-03-10 23:09:09 -0300

    Fixing Logo Homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 export default props => (
   <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
+    <Navbar.Brand as={Link} href="/">
+      Gatsby-Bootstrap
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
