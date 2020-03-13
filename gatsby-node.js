1dac58a6e6a6f6ac36579dbfb2e137d0dda30708
danielvfo <danielvfo@gmail.com>
2020-03-12 22:11:21 -0300

    gatsby-node.js fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,6 @@
 const { createFilePath } = require('gatsby-source-filesystem');
 const path = require('path');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === 'wordpress__POST') {
-    const slug = createFilePath({ node, getNode, basePath: 'posts' });
-    createNodeField({
-      node,
-      name: 'slug',
-      value: slug
-    });
-  }
-};
-
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return graphql(`
